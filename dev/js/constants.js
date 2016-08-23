6029fa0fc8c92c43c6c5839cac73deda20db881e
Arek Mano <manoukian.arek@gmail.com>
2016-08-23 00:10:17 -0400

    new key

--- dev/js/constants.js
+++ dev/js/constants.js
@@ -2,14 +2,14 @@
 
 module.exports = {
   public_key: "-----BEGIN PUBLIC KEY-----\
-               MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwZ1gwnDlQnSJGCGjHW2+\
-               TOHBu+1atNese4KV/76onWj7TQuzGpbEHpxng0pI3mbvBvb0Bcw5E7a9oUmEwWFi\
-               O6r3FLPtlz4xHApnZ12v/esWhyXqqneIuBUHPv2Vqig+GG7HEKJKqxL617scmPQ6\
-               Jk5DvIHIcIkqobguqLVvO3Lw8VNK1U9GwIh0cCV4PMXjtvZ3VVuyq1Vmupv82Jev\
-               iuO0MLHPUcnRWhFn3x1EZrAdDQjTJ1MPMVR6iIkyHAAFdesofy//VgkGK7cnN+k+\
-               QpIwnHHRKXAfqCUos9FbN6FhPjLIRWzYT5gEsYtz7QNaX2H9qecWlwALs5ni+wCR\
-               EwIDAQAB\
-               -----END PUBLIC KEY-----",
+              MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAshErgcyhp4w9ih+WmBIV\
+              Adqk9uHN0xSANpdb8QJWMYzqPptgbdjWRJsV7lR7h6nCSICcZLjT8BNd50Xen8/G\
+              XJZoZERSeEASjTRzMF9DIXbLM+9mc4EuDdwFJUMbICrtWRB8zvaWMP3Sl436hquN\
+              T5ZjnC7Kiz8uoCMkFThLIBpH9IP6USJuw/ikAFXoXwXPZJSTIfb2REIKpfqq4zAZ\
+              tkmH+NNT+qokhSmsQmIdkdEH64svdR1eV7ggcaOFIbFSmHSRy3Ck/41upJt5d58Q\
+              Wdfe25kUhCm0kljoVhexUYNKeoiTrLn/XvABmkJt4C0VGO3WqMtYocehcBSg4yEO\
+              bQIDAQAB\
+              -----END PUBLIC KEY-----",
   value_options: [
     101,
     201,
@@ -19,4 +19,4 @@ module.exports = {
     601,
     605
   ]
-}
+};
