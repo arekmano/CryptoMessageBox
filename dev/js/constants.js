d4b7cc0a9baf410ca82c0b29db1a49ec25bcec4e
Arek Mano <manoukian.arek@gmail.com>
2016-08-23 20:32:44 -0400

    reset pub

--- dev/js/constants.js
+++ dev/js/constants.js
@@ -2,13 +2,18 @@
 
 module.exports = {
   public_key: "-----BEGIN PUBLIC KEY-----\
-              MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAshErgcyhp4w9ih+WmBIV\
-              Adqk9uHN0xSANpdb8QJWMYzqPptgbdjWRJsV7lR7h6nCSICcZLjT8BNd50Xen8/G\
-              XJZoZERSeEASjTRzMF9DIXbLM+9mc4EuDdwFJUMbICrtWRB8zvaWMP3Sl436hquN\
-              T5ZjnC7Kiz8uoCMkFThLIBpH9IP6USJuw/ikAFXoXwXPZJSTIfb2REIKpfqq4zAZ\
-              tkmH+NNT+qokhSmsQmIdkdEH64svdR1eV7ggcaOFIbFSmHSRy3Ck/41upJt5d58Q\
-              Wdfe25kUhCm0kljoVhexUYNKeoiTrLn/XvABmkJt4C0VGO3WqMtYocehcBSg4yEO\
-              bQIDAQAB\
+              MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA5nip3X6DGvRnTCF7hQko\
+              Tggz3q1MUNXVbDgacR9daeINJJ+iKUqGM4N2ipJsaKdaX2OnwFqOca/Wh08Dqrfu\
+              A6uWmusrQnK4mhMRz4eLT4NdITUe/F0rEyUwVGjTC4YSzacBCeJJs731JGiFO7Zk\
+              bbxPHUAgu0tDk5KBnDYXJ647fniBQxNKE9vYDt8LcYfv+Xa/np3o/vWaahGQT41h\
+              TaudLDDbMce3vHNnM4bIHeajrJ9flzco7sZ4WhgmGZ1dFt4h0T4+w1ACf3+s39hp\
+              pHe7jGr5WT1GOqFvrrJYJ7hD1QPCKfSpxX14btHx6KyHPb/Jtnob/1h1EXmaV4F9\
+              dtr9OggRfEQd7HRpBIPHqTE/L4sgfoZlaeyuHTU+tg+vXa/y2b0tuXm8cTVEEWZf\
+              4s6esLKDOeZ6ZjJDZ0G3IG9VV1FAgeMqbkEwhcxEt+WeafMJC0Wd/NhVIHFWZSw3\
+              NgOvLsQmcQy1MnadvpmPtjDvQXCQPnGAdKUEuotHWVaF+p06XxKeZ9k2BzQ4RLPA\
+              xZOUQ+HQNxHJN3vgqDyb6nrRWrmRpFW6fD9im8wfcJxO77ySduCDq+hcHk7UqvnL\
+              I/6EcMujiI0Ryg0tnQteAU9CF6+xVyHpYASHo0t5SZDm1bKdWrjkJUsPzpdjzc1/\
+              zY6+FSxBhtvkJ+egyCKdMhsCAwEAAQ==\
               -----END PUBLIC KEY-----",
   value_options: [
     101,
